b06525638014cd912d0d8b7a5bc772177f6b9533
Terry Goodwin <terrygoodwin1984@googlemail.com>
2021-03-28 14:08:10 +0100

    Updated master script to reflect all the recent changes

--- run_me_first_after_readme.bat
+++ run_me_first_after_readme.bat
@@ -5,11 +5,34 @@ set THIS_PATH=%CD%
 
 echo NMMAMP-ExtraMuseum Untethered Boot
 echo ----------------------------------
-echo Version 0.1.3.0 by Terry Goodwin
+echo Version 0.1.4.0 by Terry Goodwin
 echo ----------------------------------
 echo Android tools path: %ADB_FOLDER%
 echo Running from path: %THIS_PATH%
 
+echo.
+echo This will install the launcher, RetroArch, and all other data.
+echo Please make sure your device is turned on, waiting at the game select
+echo screen, and it is plugged into your computer via USB.
+echo.
+echo All of this can easily be reversed by running the "remove_all.bat"
+echo script, which will return everything to how it was.
+echo.
+echo These scripts do not modify any of the existing data on your device,
+echo they just add some more stuff and modify one single configuration
+echo setting.
+echo.
+echo WARNING! Please make sure no other Android devices are connected!
+echo.
+set /p CONFIRM="Proceed with installation? (y/n): "
+
+if "%CONFIRM%"=="y" (goto proceed)
+if "%CONFIRM%"=="yes" (goto proceed)
+if "%CONFIRM%"=="Y" (goto proceed)
+if "%CONFIRM%"=="YES" (goto proceed)
+goto:aborted
+
+:proceed
 echo.
 echo Getting devices with ADB, will start daemon if it needs to...
 call %ADB_FOLDER%\adb devices || goto:devicesfailed
@@ -48,6 +71,7 @@ echo Pushing purpose-built RetroArch config into place...
 call %ADB_FOLDER%\adb push %THIS_PATH%\retroarch\retroarch.cfg /mnt/media_rw/sdcard/Android/data/com.retroarch.ra32/files/retroarch.cfg || goto:configfailed
 echo Config push success
 
+:retroarchoverrides
 echo.
 echo Pushing purpose-built RetroArch core overrides into place...
 call %ADB_FOLDER%\adb push %THIS_PATH%\retroarch\config /mnt/sdcard/RetroArch/ || goto:overridesfailed
@@ -59,11 +83,25 @@ echo Copying over RetroArch cores...
 call %ADB_FOLDER%\adb push %THIS_PATH%\retroarch\cores /data/data/com.retroarch.ra32/ || goto:coresfailed
 echo Cores push success
 
+:playlists
+echo.
+echo Pushing RetroArch playlists into place...
+call %ADB_FOLDER%\adb push %THIS_PATH%\retroarch\playlists /mnt/sdcard/RetroArch/ || goto:playlistsfailed
+echo Playlists push success
+
 :roms
 echo.
-echo Copying over BIOS/ROMs etc....
-call %ADB_FOLDER%\adb push %THIS_PATH%\GAME-EXTRA /system/media/ || goto:romsfailed
+echo Copying over ROMs...
+call %ADB_FOLDER%\adb push %THIS_PATH%\roms /data/data/com.retroarch.ra32 || goto:romsfailed
 echo ROMs push success
+
+:bios
+echo.
+echo Copying over BIOS...
+call %ADB_FOLDER%\adb push %THIS_PATH%\bios /mnt/sdcard/RetroArch || goto:biosfailed
+call %ADB_FOLDER%\adb shell cp /mnt/sdcard/RetroArch/bios/* /mnt/sdcard/RetroArch/ || goto:biosfailed
+call %ADB_FOLDER%\adb shell rm -r /mnt/sdcard/RetroArch/bios || goto:biosfailed
+echo BIOS push success
 
 :thumbnails
 echo.
@@ -77,6 +115,31 @@ echo Installing emlauncher.apk from %THIS_PATH%\frontend\emlauncher.apk
 call %ADB_FOLDER%\adb install %THIS_PATH%\frontend\emlauncher.apk || goto:installlauncherfailed
 echo Success - emlauncher.apk installed (or was already installed)
 
+:gamelist
+echo.
+echo Pushing game list into place...
+call %ADB_FOLDER%\adb push %THIS_PATH%\frontend\gamelist.json /data/data/com.tgoodwin.emlauncher/ || goto:listfailed
+echo Gamelist push success
+
+:screenshots
+echo.
+echo Pushing screenshots into place...
+call %ADB_FOLDER%\adb push %THIS_PATH%\frontend\screenshots /data/data/com.tgoodwin.emlauncher/ || goto:screenshotsfailed
+echo Screenshots push success
+
+:systems
+echo.
+echo Pushing system icons into place...
+call %ADB_FOLDER%\adb push %THIS_PATH%\frontend\systems /data/data/com.tgoodwin.emlauncher/ || goto:systemsfailed
+echo Playlists push success
+
+:emthumbnails
+echo.
+echo Pushing thumbnails into place...
+call %ADB_FOLDER%\adb push %THIS_PATH%\frontend\thumbnails /data/data/com.tgoodwin.emlauncher/ || goto:emthumbnailsfailed
+echo Thumbnails push success
+
+:packagexml
 echo.
 echo Pushing clean package restrictions XML...
 call %ADB_FOLDER%\adb push %THIS_PATH%\config\package-restrictions.stock.xml /data/system/users/0/package-restrictions.xml || goto:packagesfailed
@@ -88,7 +151,8 @@ call %ADB_FOLDER%\adb reboot
 
 echo.
 echo Waiting for 45 seconds for device to reboot...
-echo PLEASE be patient!
+echo Installation will resume automatically soon.
+echo PLEASE be patient, and don't touch anything!
 ping 192.0.2.2 -n 1 -w 45000 > delete_me
 
 echo.
@@ -117,7 +181,7 @@ goto:retroarchconfig
 echo.
 echo Failed to push special RetroArch config - is it in the right place with the right name (retroarch\retroarch.cfg)?
 echo It might be okay if this fails, but if this is your first time running this you won't be able to control the RetroArch menus...
-goto:retroarchcores
+goto:retroarchoverrides
 
 :overridesfailed
 echo.
@@ -129,12 +193,23 @@ goto:retroarchcores
 echo.
 echo Failed to push RetroArch cores - has the directory (retroarch\cores) been moved or deleted?
 echo It might be okay if this fails, but if this is your first time running this you might not have any cores available...
+goto:playlists
+
+:playlistsfailed
+echo.
+echo Failed to push playlists - is the retroarch\playlists folder where it should be?
 goto:roms
 
 :romsfailed
 echo.
 echo Failed to push BIOS/ROMs etc. - has the directory (GAME-EXTRA) been moved or deleted?
 echo It might be okay if this fails, but if this is your first time running this you might not have any ROMs available...
+goto:bios
+
+:biosfailed
+echo.
+echo Failed to push BIOS files - has the directory (bios) been moved or deleted?
+echo It might be okay if this fails, but if this is your first time running this you might not have any BIOS files available...
 goto:thumbnails
 
 :thumbnailsfailed
@@ -146,6 +221,26 @@ goto:launcher
 echo.
 echo Failed to install launcher APK - is it in the right place with the right name (frontend\emlauncher.apk)?
 goto:failed
+
+:listfailed
+echo.
+echo Failed to push game list json- is it in the right place with the right name (frontend\gamelist.json)?
+goto:screenshots
+
+:screenshotsfailed
+echo.
+echo Failed to push screenshots - is the frontend\screenshots folder where it should be?
+goto:systems
+
+:systemsfailed
+echo.
+echo Failed to push system icons - is the frontend\systems folder where it should be?
+goto:emthumbnails
+
+:emthumbnailsfailed
+echo.
+echo Failed to push thumbnails - is the frontend\thumbnails folder where it should be?
+goto:packagexml
 
 :packagesfailed
 echo.
@@ -178,6 +273,11 @@ echo Finished with errors - things may not have worked. Resolve any errors, and 
 goto:endpause
 
 REM ----------------------------------- The End -----------------------------------
+
+:aborted
+echo.
+echo Aborted, did nothing
+goto:endpause
 
 :end
 echo.
